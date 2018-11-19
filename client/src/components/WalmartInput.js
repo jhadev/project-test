cff8411f3322086a75225dade830259b2f79693c
KI <iqbal.mohammadk@gmail.com>
2018-11-19 09:26:24 -0500

    saidenav

---
+++ client/src/components/WalmartInput.js
@@ -0,0 +1,15 @@
+const Example = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>card subtitle</CardSubtitle>
+          <CardText>card text</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
