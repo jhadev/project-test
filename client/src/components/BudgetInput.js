4a46bb80ee9a6de337f418d18d7f75ac333f0533
KI <iqbal.mohammadk@gmail.com>
2018-11-20 14:57:39 -0500

    sidenav

--- client/src/components/BudgetInput.js
+++ client/src/components/BudgetInput.js
@@ -1,4 +1,4 @@
-const Example = (props) => {
+const Budget = (props) => {
   return (
     <div>
       <Card>
@@ -6,7 +6,7 @@ const Example = (props) => {
         <CardBody>
           <CardTitle>Card title</CardTitle>
           <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <CardText>..text..</CardText>
           <Button>Button</Button>
         </CardBody>
       </Card>
